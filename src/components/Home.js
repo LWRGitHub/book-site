33493aa622cb8a276caa826ca3f0bb2560b1f1f4
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-06-05 23:16:10 -0700

    site made usable tell I find time to work on it

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Home = () => (
     <div className='text-center'>
-        <NavLink to="/books"><img className="img-fluid mb-4" src='images/homePageBooks.png' /></NavLink>
+        <NavLink to="/books"><img className="img-fluid mb-4"  src={process.env.PUBLIC_URL + '/images/homePageBooks.png'}/></NavLink>
         <p className="my-5" style={{fontSize: '20px'}}>Welcome, and thank you for visiting! Here at the Midnight Dream Publishing we dedicate ourselves to bringing you quality book in genres you are look for. Are books range form Finance books to kids books and on onward. We invite you to take a look at our <a href='https://www.amazon.com/Logan-Reynolds/e/B089CBVN5F'>Amazon store</a>, also feel free to brows or search are <NavLink to="/books">library.</NavLink> Once again thank you for visiting, and if you enjoy reading our books, please feel free to leave a good review on one of are books in are <a href='https://www.amazon.com/Logan-Reynolds/e/B089CBVN5F'>Amazon store.</a></p> 
     </div>
 );
--- src/components/genres/All.js
+++ src/components/genres/All.js
@@ -5,23 +5,23 @@ const Finaces = () => (
         <div class="row row-cols-1 row-cols-md-3">
             <div class="col mb-4">
                 <div class="card">
-                <img src="images/dropship.png" class="card-img-top" alt="Make money with drop shipping audiobook cover delivery man delivering package somewhere in the city, The big smile on his face he's wearing a blue hat and a blue collared shirt." />
+                <img src={process.env.PUBLIC_URL + '/images/dropship.png'} class="card-img-top" alt="Make money with drop shipping audiobook cover delivery man delivering package somewhere in the city, The big smile on his face he's wearing a blue hat and a blue collared shirt." />
                 <div class="card-body">
                     <h5 class="card-title">Make Money With Drop Shipping</h5>
-                    <p class="card-text">...</p>
+                    {/* <p class="card-text">...</p> */}
                 </div>
                 </div>
             </div>
             <div class="col mb-4">
                 <div class="card">
-                <img src="images/Get-a-real-estate-life.png" class="card-img-top" alt="..." />
+                <img src={process.env.PUBLIC_URL + '/images/Get-a-real-estate-life.png'} class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">Get a (Real Estate) Life!</h5>
-                    <p class="card-text">...</p>
+                    {/* <p class="card-text">...</p> */}
                 </div>
                 </div>
             </div>
-            <div class="col mb-4">
+            {/* <div class="col mb-4">
                 <div class="card">
                 <img src="..." class="card-img-top" alt="..." />
                 <div class="card-body">
@@ -29,17 +29,17 @@ const Finaces = () => (
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
                 </div>
                 </div>
-            </div>
+            </div> */}
             <div class="col mb-4">
                 <div class="card">
-                <img src="images/9-year-old-hero.png" class="card-img-top" alt=" Image of nine-year-old hero audio book cover. By Logan Reynolds through the midnight dream publishing. Red head in a superhero costume nine-year-old kid. Blue cape red shirt white clothes socks in the living room." />
+                <img src={process.env.PUBLIC_URL + '/images/9-year-old-hero.png'} class="card-img-top" alt=" Image of nine-year-old hero audio book cover. By Logan Reynolds through the midnight dream publishing. Red head in a superhero costume nine-year-old kid. Blue cape red shirt white clothes socks in the living room." />
                 <div class="card-body">
                     <h5 class="card-title">The 9 Year Old Hero</h5>
-                    <p class="card-text">...</p>
+                    {/* <p class="card-text">...</p> */}
                 </div>
                 </div>
             </div>
-            </div>
+        </div>
     </div>
 )
 
