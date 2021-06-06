13f55c959f2f1dd8d08ee81043e68e9126dc7675
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-06-05 22:51:32 -0700

    Updates

---
+++ precache-manifest.5b4d7af5b1eb6f3752d85ddf97fea976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f759f3e5ca7cf3e4bd775b3e4402fbbb",
+    "url": "/book-site/index.html"
+  },
+  {
+    "revision": "173bb954b7c008606a4a",
+    "url": "/book-site/static/css/2.be04442c.chunk.css"
+  },
+  {
+    "revision": "c812e77076692915f45a",
+    "url": "/book-site/static/css/main.0b5d8d96.chunk.css"
+  },
+  {
+    "revision": "173bb954b7c008606a4a",
+    "url": "/book-site/static/js/2.e8396b08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-site/static/js/2.e8396b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c812e77076692915f45a",
+    "url": "/book-site/static/js/main.a5c36a51.chunk.js"
+  },
+  {
+    "revision": "dfb0382ec5f0436adfe9",
+    "url": "/book-site/static/js/runtime-main.d870406e.js"
+  }
+]);
